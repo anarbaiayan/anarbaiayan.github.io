765b840d6911798f15521ba9f92ededc6e463561
Ayan <anarbajaan1@gmail.com>
2022-01-23 20:55:11 +0600

    add new project

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    let owl = $(".owl-carousel")
+
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        dots: true,
+    });
+
+    $('.arrow.prev').on('click', function () {
+        owl.trigger('prev.owl.carousel')
+    })
+    
+    $('.arrow.next').on('click', function () {
+        owl.trigger('next.owl.carousel')
+    })
+
+});
